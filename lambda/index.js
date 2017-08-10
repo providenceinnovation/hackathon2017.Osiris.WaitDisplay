44eb016fb7b2bd1dec54ae3ff9ed9e1dff2e3bb5
Stevi Deter <stevi.deter@providence.org>
2017-08-10 16:29:36 -0700

    reset

--- lambda/index.js
+++ lambda/index.js
@@ -1,56 +1,80 @@
+'use strict';
+
 let https = require('https');
 const firebaseHost = 'osiris-26b00.firebaseio.com';
 const testProviderId = 'wa2000test';
-let getCurrentWaitTime = function (providerId) {
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const RESET = 'RESET';
+let getCurrentWaitTime = function (providerId, requestType, callback) {
   console.log(`getting wait time for ${providerId}`);
   https.get(`https://${firebaseHost}/realtime/${providerId}/waitTime/value.json`, (res) => {
     console.log('statusCode:', res.statusCode);
     res.on('data', (d) => {
       process.stdout.write(d + '\n');
       var currentValue = parseInt(d.toString());
-      let newValue = 0;
-      if (!isNaN(currentValue) && currentValue <= 50) {
-        newValue = currentValue + 10;
+      let newValue = isNaN(currentValue) ? 0 : currentValue;
+      if (requestType === INCREMENT) {
+        newValue += 10;
+      } else {
+        newValue -= 10;
       }
-      setNewWaitTime(providerId, newValue);
+      setNewWaitTime(providerId, newValue, callback);
     });
 
   }).on('error', (e) => {
     console.error(e);
+    callback(e, null);
   });
 };
 
-let setNewWaitTime = function (providerId, newValue) {
+let setNewWaitTime = function (providerId, newValue, callback) {
   console.log(`updating wait time for provider ${providerId}`);
   let options = {
     hostname: firebaseHost,
     port: 443,
     path: `/realtime/${providerId}/waitTime/value.json`,
     method: 'PUT'
-    // ,
-    // headers: {
-    //   'Content-Type': 'application/json'
-    // }
   };
 
   let req = https.request(options, function (res) {
-    var str = ''
+    var str = '';
     res.on('data', function (chunk) {
       str += chunk
-    })
+    });
 
     res.on('end', function () {
-      console.log(str)
+      console.log(str);
+      callback(null, newValue);
     })
-
+    res.on('error', (e) => {
+      console.error(e);
+      callback(e, null);
+    });
   });
 
   var body = newValue.toString();
-  // JSON.stringify({
-  //   value: newValue
-  // });
 
   req.end(body);
 
 };
-getCurrentWaitTime(testProviderId);
+
+exports.handler = (event, context, callback) => {
+  console.log('Received event:', event);
+  let requestType;
+  switch (event.clickType) {
+    case 'DOUBLE':
+      requestType = DECREMENT;
+      break;
+    case 'LONG':
+      requestType = RESET;
+      break;
+    default:
+      requestType = INCREMENT;
+  }
+  if (requestType === RESET) {
+    setNewWaitTime(testProviderId, 0, callback);
+  } else {
+    getCurrentWaitTime(testProviderId, requestType, callback);
+  }
+};
